8781d68e97680bb6771c40db3955ef5aaef1f3b0
AmoghChhuneja <amoghchhunejaofficial@gmail.com>
2026-08-08 15:24:05 +0530

    Remove unnecessary file

--- slicing.py
+++
@@ -1,9 +0,0 @@
-piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
-piano_tuple = ("do", "re", "mi", "fa", "so", "la", "ti")
-
-# The original image had an incomplete print statement: print(piano_keys[])
-# You need to provide an index number inside the brackets to fix the error:
-print(piano_keys[2:])
-print(piano_keys[:5])
-print(piano_keys[1:5:2])
-print(piano_keys[::-1])
